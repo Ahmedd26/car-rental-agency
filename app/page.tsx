76ea204de8cd939d68797823303f141174259af7
Ahmedd26 <ahmedabdellatife1@gmail.com>
2025-01-23 06:23:57 +0200

    'Added popular cars  section title and view all cars link in the home page '

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 import Hero from "@/app/_components/home/Hero";
 import Filter from "@/app/_components/Filter/Filter";
 import CarGrid from "@/app/_components/CarGrid";
+import Link from "next/link";
 
 export default function Home() {
   return (
@@ -10,6 +11,17 @@ export default function Home() {
         <Filter />
       </div>
       <div className="my-8 px-6 lg:px-16">
+        <div className="mb-5 flex items-center justify-between sm:mb-6 md:mb-8">
+          <h3 className="text-sm font-semibold text-secondary-300 sm:text-base">
+            Popular Car
+          </h3>
+          <Link
+            href="/cars"
+            className="text-xs font-semibold text-primary-500 sm:text-sm md:text-base"
+          >
+            View All
+          </Link>
+        </div>
         <CarGrid />
       </div>
     </main>
