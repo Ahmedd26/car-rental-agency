4fe0251bd33314be741ec8b66ca61e01feedaeaa
Ahmedd26 <ahmedabdellatife1@gmail.com>
2025-02-09 14:54:31 +0200

    Added formatNumber Utility function to format numbers for more human readable format (i.e., 10200 => 10.2K)

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,6 +1,16 @@
-import { clsx, type ClassValue } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { clsx, type ClassValue } from "clsx";
+import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
 }
+
+export const formatNumber = (num: number, decimals = 1): string => {
+  const units = ["", "K", "M", "B", "T"];
+  const tier = (Math.log10(num) / 3) | 0;
+  if (tier === 0) return num.toString();
+  const suffix = units[tier];
+  const scale = Math.pow(10, tier * 3);
+  const scaled = num / scale;
+  return scaled.toFixed(decimals) + suffix;
+};
