986e71cdd82d1e94c67f0951180a5a9bc419ef98
Ahmedd26 <ahmedabdellatife1@gmail.com>
2025-02-09 14:55:52 +0200

    Created type "TailwindClass" to help with IDE intellisense

---
+++ types/tailwind.ts
@@ -0,0 +1,12 @@
+export type TailwindClass = string & { __tailwindClassBrand?: never };
+
+// ** USAGE ** //
+
+// interface Props {
+//   /**
+//    * @tw
+//    * Tailwind CSS classes (auto-suggested)
+//    */
+//   className?: TailwindClass;
+//   // ...rest
+// }
