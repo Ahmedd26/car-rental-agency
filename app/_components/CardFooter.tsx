23a55e4e5b3c3a8c2b9fdffcb45de487d1146662
Ahmedd26 <ahmedabdellatife1@gmail.com>
2025-02-10 06:07:12 +0200

    Fix: Fixed discount calculation by creating a separate function to handle it

--- app/_components/CardFooter.tsx
+++ app/_components/CardFooter.tsx
@@ -1,34 +1,40 @@
-import { formatNumber } from "@/lib/utils";
 import { type TailwindClass } from "@/types/tailwind";
 import Link from "next/link";
 
 interface Props {
   price: number;
   linkHref: string;
-  discountPercentage?: number;
+  discountPercentage: number;
   className?: TailwindClass;
 }
 
-function CardFooter({ price, discountPercentage, linkHref, className }: Props) {
+function CardFooter({
+  price,
+  discountPercentage = 0,
+  linkHref,
+  className,
+}: Props) {
+  function applyDiscount(price: number, discount: number) {
+    return discount > 0
+      ? Math.round(Math.abs(Math.abs(price) * (1 - Math.abs(discount) / 100)))
+      : Math.round(Math.round(Math.abs(price)));
+  }
+
   return (
     <div
       className={`${className ?? ""} mt-9 flex flex-wrap items-center justify-between gap-4 md:mt-6`}
     >
       <div className="flex flex-col">
         <span className="text-xl font-bold text-secondary-500">
-          $
-          {discountPercentage
-            ? formatNumber(price - price * (discountPercentage / 100))
-            : formatNumber(price)}
-          /
+          ${applyDiscount(price, discountPercentage)}/
           <span className="ms-0.5 text-sm font-normal text-secondary-300">
             day
           </span>
         </span>
         {/* Discount, OPTIONAL */}
-        {discountPercentage && (
+        {discountPercentage > 0 && (
           <span className="text-sm font-bold text-secondary-300 line-through">
-            ${formatNumber(price)}
+            ${price}
           </span>
         )}
       </div>
